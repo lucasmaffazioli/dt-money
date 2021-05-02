54b705f22d53775efd7dba0da9bcbff1da3154b5
djlucask <lucas.maffazioli@gmail.com>
2021-05-02 20:33:16 -0300

    Noice

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
   return (
 
     <div className="App">
-      <Header></Header>
+      <Header handleOpenTransactionModal={handleOpenTransactionModal} ></Header>
       <Summary></Summary>
 
       <Modal
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,17 @@
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
+interface HeaderProps {
+	handleOpenTransactionModal: () => void
+}
 
-export function Header() {
-
-
-
+export function Header(props: HeaderProps) {
 	return (
 		<>
 			<Container>
 				<Content>
 					<img src={logoImg} alt="DtMoney" />
-					<button type="button" onClick={handleOpenTransactionModal} >Nova transação</button>
+					<button type="button" onClick={props.handleOpenTransactionModal} >Nova transação</button>
 				</Content>
 			</Container>
 		</>
